62366f28342680dcc50c27885aa0a85992101d9f
QAZWSX12345Q-AFK <2686718691@qq.com>
2026-09-10 14:59:34 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
-  async headers() {
-    return [
-      {
-        source: "/manifest.webmanifest",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/manifest+json",
-          },
-        ],
-      },
-    ];
+  output: "export",
+  basePath: "/0910-1",
+  assetPrefix: "/0910-1/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
